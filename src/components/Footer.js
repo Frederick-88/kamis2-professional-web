03157edf96901300c660b40250427d1fdf76bee8
Frederick-88 <frederickfd88@gmail.com>
2020-04-19 23:07:00 +0700

    MAKING EMAIL FORM

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from "react";
+import axios from "axios"
 import "./style.css"
 
 const NavbarUtama = () => {
+  
   return (
     <div className="footer text-center">
             DESIGN AND DEVELOPED BY FD.TECHNOLOGIES
+            <div>
+
+            </div>
         </div>
   );
 };
